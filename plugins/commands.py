280c153e4603ebbe14f33dae6c6722d4e9b6492f
Muhammed Aflah <aflahtbm@gmail.com>
2025-09-04 20:35:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -204,9 +204,7 @@ async def start(client, message):
             InlineKeyboardButton('ʀᴇᴘᴏ', url='https://github.com/mn-bots/ShobanaFilterBot')
          ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_text("ShobanaFilterBot") 
-        await asyncio.sleep(1.2)
-        await m.delete()
+        
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
